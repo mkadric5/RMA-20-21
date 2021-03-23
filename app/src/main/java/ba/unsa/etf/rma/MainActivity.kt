bdcfb59cd67ab2c7d8fc4de98cfef203bb9ac004
mkadric5 <mkadric5@etf.unsa.ba>
2021-03-23 20:25:57 +0100

    Ispravljene greske prethodno napravljene - za template nije uzeta bila empty activity

---
+++ app/src/main/java/ba/unsa/etf/rma/MainActivity.kt
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnApply = findViewById<Button>(R.id.apply)
+
+        btnApply.setOnClickListener {
+            showMessage()
+        }
+    }
+
+    private fun showMessage() {
+        val message = findViewById<EditText>(R.id.unos).text.toString()
+        findViewById<TextView>(R.id.ispis).text=message
+    }
+}
+
